893d5c7960f8e92838fe944d2e98d68d22da964c
bickle1 <110621930+bickle1@users.noreply.github.com>
2022-08-18 17:14:32 +0100

    Add files via upload

--- main-window.py
+++ main-window.py
@@ -1,33 +1,36 @@
 from tkinter import *
+from tkinter import ttk
+
+from tkcalendar import DateEntry
+
 import ftplib
 ftp = ftplib.FTP()
 
 
+# FTP commands =====================
+# Attempts to establish a connection to the ftp server with the given details
 def connect_server():
-    pass
-    ip = ent_ip.get()
-    port = int(ent_port.get())
+    ip = "127.0.0.1"#ent_ip.get()
+    port = 21#int(ent_port.get())
     try:
        msg = ftp.connect(ip,port)
-       text_servermsg.insert(END,"\n")
-       text_servermsg.insert(END,msg)
+       text_servermsg.insert(END, "\n" + msg)
        lbl_login.place(x=150,y=20)
        ent_login.place(x=150,y=40)
        lbl_pass.place(x=150,y=60)
        ent_pass.place(x=150,y=80)
        btn_login.place(x=182,y=110)
     except:
-       text_servermsg.insert(END,"\n")
-       text_servermsg.insert(END,"Unable to connect")
+       text_servermsg.insert(END,"\nUnable to connect")
 
 
+# Logs the user in with the given credentials
 def login_server():
-    user = ent_login.get()
-    password = ent_pass.get()
+    user = "testUser"#ent_login.get()
+    password = "Pa$$w0rd"#ent_pass.get()
     try:
         msg = ftp.login(user,password)
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,msg)
+        text_servermsg.insert(END, "\n" + msg)
         display_dir()
         lbl_login.place_forget()
         ent_login.place_forget()
@@ -35,154 +38,168 @@ def login_server():
         ent_pass.place_forget()
         btn_login.place_forget()
     except:
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,"Unable to login")
+        text_servermsg.insert(END, "\nUnable to login")
 
-        
+
+# Each time the active directory changes, the list is cleared and the new files are displayed
 def display_dir():
     libox_serverdir.delete(0, END)
     libox_serverdir.insert(0,"--------------------------------------------")
-    dirlist = []
     dirlist = ftp.nlst()
+    dirlist.reverse() # Sorts directory listing to descending alphabetical
     for item in dirlist:
         libox_serverdir.insert(0, item)
 
 
-#FTP commands
+# Changes the active server directory currently being viewed
 def change_directory():
-    directory = ent_input.get()
+    try:
+        directory = libox_serverdir.get(libox_serverdir.curselection())
+    except:
+        directory = ""
+
     try:
         msg = ftp.cwd(directory)
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,msg)
+        text_servermsg.insert(END, "\n" + msg)
     except:
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,"Unable to change directory")
+        text_servermsg.insert(END, "\nUnable to change directory")
     display_dir()
 
 
-def create_directory():
-    directory = ent_input.get()
+# Steps up to the parent server directory
+def step_up_directory():
+    directory = ".."
     try:
-        msg = ftp.mkd(directory)
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,msg)
+        msg = ftp.cwd(directory)
+        text_servermsg.insert(END, "\n" + msg)
     except:
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,"Unable to create directory")
+        text_servermsg.insert(END, "\nUnable to change directory")
     display_dir()
 
 
+# Downloads the single file selected in the server directory listing
 def download_file():
-    file = ent_input.get()
+    file = libox_serverdir.get(libox_serverdir.curselection())
+
     down = open(file, "wb")
     try:
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,"Downloading " + file + "...")
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,ftp.retrbinary("RETR " + file, down.write))
+        text_servermsg.insert(END, "\nDownloading " + file + "...\n")
+        text_servermsg.insert(END, ftp.retrbinary("RETR " + file, down.write))
     except:
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,"Unable to download file")
+        text_servermsg.insert(END, "\nUnable to download file")
     display_dir()
 
 
-def create_schedule_window():
-    # ======== SCHEDULE WINDOW SETUP ========
-    schedule = Toplevel(window)
-    schedule.title("FTP Client - Set up Schedule")
-    schedule.geometry("300x200")
+# Downloads all files that were created on a given date
+def download_files_from_date():
+    pass
 
-    # Repeat Options
-    # repeate_type: 0-Days | 1-Weeks | 2-Months
-    repeat_type = 0
-
-    spn_repeat_value = Spinbox(schedule, width=5)
-    lbl_repeatmsg1 = Label(schedule, text="Repeat every: ")
-    lbl_repeatmsg2 = Label(schedule, text="days")
-
-    # IDK where the combobox went but i need it so ill need to find out where the fuck it went and who took it then find their house and intimidate them with my towering stature and greek god like physique into putting the comboboxes back into tkinter
-    # cmb_repeat_type = Combobox(schedule, values=["days", "weeks", "months"])
-    
-    # rad_daily = Radiobutton(schedule, text="Daily", variable=repeat_type, value=0, command=lbl_repeatmsg2.config(text="days"))
-    # rad_weekly = Radiobutton(schedule, text="Weekly", variable=repeat_type, value=1, command=lbl_repeatmsg2.config(text="weeks"))
-    # rad_monthly = Radiobutton(schedule, text="Monthly", variable=repeat_type, value=2, command=lbl_repeatmsg2.config(text="months"))
-
-    # Placing Widgets
-    # rad_daily.place(x=20, y=40)
-    # rad_weekly.place(x=20, y=60)
-    # rad_monthly.place(x=20, y=80)
-    
-    
-    # cmb_repeat_type.place(x=100, y=100)
-    spn_repeat_value.place(x=180, y=160)
-    lbl_repeatmsg1.place(x=100, y=160)
-    lbl_repeatmsg2.place(x=230, y=160)
-    # ==========================================
-
-
-def set_schedule():
-    create_schedule_window()
-
-    
-    # If there is no batch file, create it.
-    # Then create the scheduled task using schtasks.
-    # os.system(f'SchTasks /Create /SC DAILY /TN "My Task" /TR "task.bat" /ST 09:00')
-
-
-def update_repeat_text(label_name, value):
-    label_name.text=value
-
-
+# Closes FTP server connection
 def close_connection():
     try:
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,"Closing connection...")
-        text_servermsg.insert(END,"\n")
+        text_servermsg.insert(END,"\nClosing connection...\n")
         text_servermsg.insert(END,ftp.quit())
     except:
-        text_servermsg.insert(END,"\n")
-        text_servermsg.insert(END,"Unable to disconnect")
+        text_servermsg.insert(END,"\nUnable to disconnect")
+# ==================================
 
 
+# Scheduling Methods ===============
+# Creates a new window with options for setting a scheduled task.
+def create_schedule_window():
+    schedule = Toplevel(window)
+    schedule.title("FTP Client - Scheduler")
+    schedule.geometry("300x200")
+
+    # Defining Widgets ============
+    lbl_start_date = Label(schedule, text="Start Date")
+    dent_start_date = DateEntry(schedule, width=12, background="darkblue", foreground="white", borderwidth=2, textvariable=start_date)
+
+    lbl_start_time = Label(schedule, text="Start Time")
+    lbl_start_hour = Label(schedule, text="Hour")
+    lbl_start_minute = Label(schedule, text="Min")
+    spn_schedule_start_hour = Spinbox(schedule, width=2, wrap=True, from_=0, to=23, textvariable=start_hour)
+    spn_schedule_start_minute = Spinbox(schedule, width=2, wrap=True, from_=0, to=59, textvariable=start_minute)
+
+    lbl_repeat_delay = Label(schedule, text="Repeat Delay")
+    cmb_repeat_delay = ttk.Combobox(schedule, values=["Daily", "Weekly", "Monthly"], width=8, textvariable=repeat_delay)
+    cmb_repeat_delay.current(0) # Sets default value to "Daily"
+
+    btn_schedule_submit = Button(schedule, text="Submit", command=submit_schedule)
+    # =============================
+    
+
+    # Placing Widgets =============
+    lbl_start_date.place(x=10, y=10)
+    dent_start_date.place(x=10, y=30)
+
+    lbl_repeat_delay.place(x=120, y=130)
+    cmb_repeat_delay.place(x=200, y=130)
+
+    lbl_start_time.place(x=120, y=10)
+    lbl_start_hour.place(x=120, y=30)
+    lbl_start_minute.place(x=200, y=30)
+    spn_schedule_start_hour.place(x=155, y=30)
+    spn_schedule_start_minute.place(x=230, y=30)
+
+    btn_schedule_submit.place(x=240, y=160)
+    # =============================
+
+
+# Triggered when the user presses the submit button when setting up a schedule
+def submit_schedule():
+    print(start_date.get(), start_hour.get(), start_minute.get(), repeat_type.get(), repeat_delay.get())
+    # If there is no batch file, create it. ?? I think (:
+    # Then create the scheduled task using schtasks. https://docs.microsoft.com/en-us/windows/win32/taskschd/schtasks
+    # os.system(f'SchTasks /Create /SC DAILY /TN "My Task" /TR "task.bat" /ST 09:00')
+# ==================================
 
 
 # ======== MAIN WINDOW SETUP ========
 window = Tk()
 window.title("FTP Client")
-window.geometry("1000x600")
+window.geometry("970x560")
 
-#Connect
+# Defining Variables =======================
+start_date = StringVar()
+start_hour = StringVar()
+start_minute = StringVar()
+repeat_type = StringVar()
+repeat_delay = StringVar()
+# ==========================================
+
+# Connect
 lbl_ip = Label(window, text="IP Address")
 ent_ip = Entry(window)
 lbl_port = Label(window, text="Port")
 ent_port = Entry(window)
 btn_connect = Button(window, text="Connect", command=connect_server)
 
-#Server response text box
+# Server response text box
 text_servermsg = Text(window)
 
-#Login
+# Login
 lbl_login = Label(window, text="Username")
 ent_login = Entry(window)
 lbl_pass = Label(window, text="Password")
 ent_pass = Entry(window)
 btn_login = Button(window, text="Login", command=login_server)
 
-#Directory listing
+# Directory listing
 lbl_dir = Label(window, text="Directory listing:")
-libox_serverdir = Listbox(window,width=40,height=14)
+libox_serverdir = Listbox(window, width=40, height=16)
 
-#Options
+# Options
 lbl_input = Label(window, text="Input")
 ent_input = Entry(window)
 btn_chdir = Button(window, text="Change Directory", command=change_directory,width=15)
-btn_crdir = Button(window, text="Create Directory", command=create_directory,width=15)
+btn_updir = Button(window, text="Step Up Directory", command=step_up_directory, width=15)
 btn_downfile = Button(window, text="Download File", command=download_file,width=15)
-btn_schedule = Button(window, text="Set Schedule", command=set_schedule, width=15)
+btn_down_from_date = Button(window, text="Download Date", command=download_files_from_date, width=15)
+btn_schedule = Button(window, text="Set Schedule", command=create_schedule_window, width=15)
 btn_quit = Button(window, text="Disconnect", command=close_connection,width=15)
 
-#Place widgets
+# Place widgets
 lbl_ip.place(x=20,y=20)
 ent_ip.place(x=20,y=40)
 lbl_port.place(x=20,y=60)
@@ -193,24 +210,15 @@ text_servermsg.place(x=20,y=150)
 lbl_dir.place(x=700,y=143)
 libox_serverdir.place(x=700,y=165)
 
-btn_schedule.place(x=700,y=510)
+btn_downfile.place(x=700,y=450)
+btn_down_from_date.place(x=700,y=480)
+btn_schedule.place(x=700, y=510)
 
-lbl_input.place(x=700,y=400)
-ent_input.place(x=700,y=420)
-btn_chdir.place(x=700,y=450)
-btn_crdir.place(x=700,y=480)
-
-btn_downfile.place(x=850,y=450)
-btn_quit.place(x=850,y=510)
+btn_updir.place(x=830, y=450)
+btn_chdir.place(x=830,y=480)
+btn_quit.place(x=830,y=510)
 # ==========================================
 
 
-
-
-
-
-
-
-
-#Create
+# Create
 window.mainloop()
