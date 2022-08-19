33483a553295715ff36bfa09fc3a8dd91d01f34f
PigeonCodes88 <95762747+PigeonCodes88@users.noreply.github.com>
2022-08-19 13:05:08 +0100

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import logging
-from csv_checker import *
-
-# sets up logging in file csv.log
-logging.basicConfig(filename='csv.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
